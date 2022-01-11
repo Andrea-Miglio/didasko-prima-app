b352e527ec26f0b7804fb86f43a92cdeb678ea7d
Andrea-Miglio <migand85@gmail.com>
2022-01-11 19:13:40 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  titolo: string = 'Data:';
+  date: Date;
+  nome: string;
+  cognome: string;
+  eta: string;
+  constructor() {
+    this.date = new Date();
+  }
+  invioDati(nome: string, cognome: string, eta: string) {
+    this.nome = nome;
+    this.cognome = cognome;
+    this.eta = eta;
+  }
 }
